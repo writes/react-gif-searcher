c90e0b6b4164d222f2dd153f0a9c2aa1bf742586
writes <jonathonhthompson@gmail.com>
2018-03-19 03:13:20 -0500

    feat(containers): create search_bar container

--- app/src/App.js
+++ app/src/App.js
@@ -3,17 +3,7 @@ import styled from 'styled-components';
 import logo from './logo.svg';
 import './App.css';
 
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
-
-// Create a Wrapper component that'll render a <section> tag with some styles
-const Wrapper = styled.section`
-  padding: 4em;
-  background: papayawhip;
-`;
+import SearchBar from './containers/search_bar';
 
 // Use Title and Wrapper like any other React component – except they're styled!
 class App extends Component {
@@ -22,9 +12,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React with REDUXX</h1>
         </header>
-        <Title>Hello World, this is my first styled component!</Title>
       </div>
     );
   }

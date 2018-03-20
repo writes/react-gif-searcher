3b84021fa75772039a0c497164010840f892942f
writes <jonathonhthompson@gmail.com>
2018-03-20 15:21:21 -0500

    feat(search): add search functionality to App

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,73 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
-import logo from './logo.svg';
-import './App.css';
 
-import SearchBar from './containers/search_bar';
+import './App.css';
 
 const GIPHY_API_KEY = 'eIRj58ROhk5HPIC7FqKTJDDH4bcGYc7W';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      gifs: []
+    };
+  }
+
+  componentDidMount() {
+    this.giphySearch('spurs');
+  }
+
+  giphyFetch(url) {
+    fetch(`http://api.giphy.com${url}&api_key=${GIPHY_API_KEY}`, {
+      method: 'get'
+    })
+      .then(results => {
+        return results.json();
+      })
+      .then(search => {
+        let gifs = search.data.map(gif => {
+          return {
+            id: gif.id,
+            url: gif.images.original.url,
+            href: gif.bitly_url
+          };
+        });
+        this.setState({ gifs: gifs });
+      });
+  }
+
+  giphySearch(search) {
+    const url = `/v1/gifs/search?q=${search}`;
+    this.giphyFetch(url);
+  }
+
+  giphyTrending() {
+    const url = '/v1/gifs/trending?';
+    this.giphyFetch(url);
+  }
+
+  search() {
+    console.log(this);
+
+    const query = document.getElementById('search') || {};
+    this.giphySearch(query.value);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
+        <input id="search" type="text" />
+        <button id="searchSubmit" onClick={() => this.search()}>
+          Go
+        </button>
+        {this.state.gifs.map(gif => {
+          return (
+            <a href={gif.href} target="_blank">
+              <div key={gif.id}>
+                <img src={gif.url} alt="something" />
+              </div>
+            </a>
+          );
+        })}
       </div>
     );
   }
